b98cb7f6039921683d77db5ce494cfa70097e969
Franco Bussano <franbussano@gmail.com>
2023-05-09 22:09:22 -0300

    Change folders and Classes names

--- proyectoSalud/src/main/java/com/equipoUno/proyectoSalud/repositories/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.equipoUno.proyectoSalud.repositories;
-
-import com.equipoUno.proyectoSalud.entities.Paciente;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PacienteRepository extends CrudRepository<Paciente, String> {
-}
